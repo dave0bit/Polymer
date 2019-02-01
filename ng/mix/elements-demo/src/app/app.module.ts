9348951c59b5444d7146aabe6906637df2135c93
dave0bit <dave0bit@hotmail.com>
2019-02-01 12:02:01 +0100

    Custom Elements

--- ng/mix/elements-demo/src/app/app.module.ts
+++ ng/mix/elements-demo/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  entryComponents: [ButtonComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const customButton = createCustomElement(ButtonComponent, { injector });
+    customElements.define('custom-button', customButton);
+  }
+
+  ngDoBootstrap() {}
+
+ }
